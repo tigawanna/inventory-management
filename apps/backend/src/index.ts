73216dd4542e0adbf486b24a3ae16cf004a70a6f
tigawanna <denniskinuthiawaweru@gmail.com>
2025-01-06 08:15:24 +0300

    add express boilerplate

--- apps/backend/src/index.ts
+++ apps/backend/src/index.ts
@@ -1,16 +1,8 @@
-// src/index.ts
-import express, { type Express, type Request, type Response } from "express";
-import dotenv from "dotenv";
+import app from './app';
 
-dotenv.config();
-
-const app: Express = express();
-const port = process.env.PORT || 3000;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("Express + TypeScript Server");
-});
-
+const port = process.env.PORT || 5000;
 app.listen(port, () => {
-  console.log(`[server]: Server is running at http://localhost:${port}`);
+  /* eslint-disable no-console */
+  console.log(`Listening: http://localhost:${port}`);
+  /* eslint-enable no-console */
 });
