dec66878aa5b56c27fc5884b7e08e1e91f383503
tigawanna <denniskinuthiawaweru@gmail.com>
2025-01-08 03:49:28 +0300

    feat: update package.json dependencies and remove unused swagger-jsdoc integration from API documentation

--- apps/backend/src/api/v1/index.ts
+++ apps/backend/src/api/v1/index.ts
@@ -1,31 +1,12 @@
 import express from "express";
 import authRoute from "./auth/apis.ts";
 import inventoryRoute from "./inventory/index.ts";
-import swaggerJsdoc from "swagger-jsdoc";
 import { apiReference } from "@scalar/express-api-reference";
 import fs from "fs/promises";
 
 const router = express.Router();
 
-router.get("/docs", (req, res) => {
-  const openapiSpecification = swaggerJsdoc({
-    definition: {
-      swagger: "2.0",
-      // basePath: "/api/v1",
-      info: {
-        title: "Inventory v1 API",
-        version: "1.0.0",
-      },
-      servers: [
-        {
-          url: "/api/v1",
-        },
-      ],
-    },
-    apis: ["./src/api/v1/*.ts"],
-  });
-  res.json(openapiSpecification);
-});
+
 router.get("/openapi.json", async (req, res) => {
   try {
     const readFile = await fs.readFile("./openapi.json");
