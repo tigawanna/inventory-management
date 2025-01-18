e47d8e134a0be53245ee5b86e0bb161c3094f6b0
tigawanna <denniskinuthiawaweru@gmail.com>
2025-01-18 12:03:33 +0300

    feat: implement custom OpenAPI hook; enhance error handling and integrate with app router

--- apps/hono/src/lib/configure-open-api.ts
+++ apps/hono/src/lib/configure-open-api.ts
@@ -1,8 +1,12 @@
+import type { Hook } from "@hono/zod-openapi";
+
 import { apiReference } from "@scalar/hono-api-reference";
 
 import type { AppOpenAPI } from "./types";
 
 import packageJSON from "../../package.json" with { type: "json" };
+import HttpStatusCodes from "./status-codes";
+import { returnValidationData } from "./zod";
 
 export function configureOpenAPI(app: AppOpenAPI) {
   app.doc("/doc", {
@@ -28,3 +32,16 @@ export function configureOpenAPI(app: AppOpenAPI) {
     }),
   );
 }
+
+export const defaultHonoOpenApiHook: Hook<any, any, any, any> = (result, c) => {
+  if (!result.success) {
+    return c.json(
+      {
+        success: result.success,
+        data:returnValidationData(result.error),
+        error: result.error,
+      },
+      HttpStatusCodes.UNPROCESSABLE_ENTITY,
+    );
+  }
+};
--- apps/hono/src/lib/create-app.ts
+++ apps/hono/src/lib/create-app.ts
@@ -2,55 +2,55 @@ import { OpenAPIHono } from "@hono/zod-openapi";
 import { contextStorage } from "hono/context-storage";
 import { requestId } from "hono/request-id";
 import { notFound, serveEmojiFavicon } from "stoker/middlewares";
-import { defaultHook } from "stoker/openapi";
 
-import { authenticateUserMiddleware } from "@/middlewares/auth-middl-ware";
 import { corsHeaders } from "@/middlewares/cors-middlewares";
 import { onHonoError } from "@/middlewares/error-middleware";
 import { pinoLogger } from "@/middlewares/loggermiddleware";
 
 import type { AppBindings, AppOpenAPI } from "./types";
 
+import { defaultHonoOpenApiHook } from "./configure-open-api";
 
 export function createRouter() {
   return new OpenAPIHono<AppBindings>({
     strict: false,
-    defaultHook,
+    defaultHook: defaultHonoOpenApiHook,
   });
 }
 
 export function createApp() {
   const app = createRouter();
   app.use(requestId());
+  // @ts-expect-error its fine
   app.use(pinoLogger());
-//   app.use(async (c,next) => {
-//   c.set(
-//     'custom-logger',
-//     pino({
-//       browser: {
-//         formatters: {
-//           level(label, _number) {
-//             return { level: label.toUpperCase() };
-//           },
-//         },
-//         write: (o) => {
-//           console.log("write logger",o);
-//           // @ts-expect-error : the type is too genrric but shape matches
-//           const { time, level, msg } = o;
-//           const paddedLevel = level.padEnd(5, ' ');
-//           const requestId = c.var.requestId;
-//           console.log(`[${time}] ${paddedLevel} (${requestId}): ${msg}`);
-//         },
-//       },
-//       enabled: true,
-//       level: 'debug',
-//       timestamp: pino.stdTimeFunctions.isoTime,
-//     }),
-//   )
-//   // next()
-// }
-//   )
-  app.use((c,next)=>corsHeaders(c,next))
+  //   app.use(async (c,next) => {
+  //   c.set(
+  //     'custom-logger',
+  //     pino({
+  //       browser: {
+  //         formatters: {
+  //           level(label, _number) {
+  //             return { level: label.toUpperCase() };
+  //           },
+  //         },
+  //         write: (o) => {
+  //           console.log("write logger",o);
+  //           // @ts-expect-error : the type is too genrric but shape matches
+  //           const { time, level, msg } = o;
+  //           const paddedLevel = level.padEnd(5, ' ');
+  //           const requestId = c.var.requestId;
+  //           console.log(`[${time}] ${paddedLevel} (${requestId}): ${msg}`);
+  //         },
+  //       },
+  //       enabled: true,
+  //       level: 'debug',
+  //       timestamp: pino.stdTimeFunctions.isoTime,
+  //     }),
+  //   )
+  //   // next()
+  // }
+  //   )
+  app.use((c, next) => corsHeaders(c, next));
   app.use(contextStorage());
   // app.use(async (c, next) => {
   //   await authenticateUserMiddleware(c, next);
