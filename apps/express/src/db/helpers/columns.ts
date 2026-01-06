f456e73423a8718b7194be6a60295f3e04ab887d
tigawanna <denniskinuthiaw@gmail.com>
2026-01-06 11:32:13 +0300

    cleanup

--- apps/express/src/db/helpers/columns.ts
+++
@@ -1,9 +0,0 @@
-import { timestamp, text } from "drizzle-orm/pg-core";
-import { uuidv7 } from "uuidv7";
-export const commonColumns = {
-  id: text()
-    .$defaultFn(() => uuidv7())
-    .primaryKey(),
-  updated_at: timestamp(),
-  created_at: timestamp().defaultNow(),
-};
